f09f3153b15d9581e623845fe437c566d842359f
Naohito-T <naohito.tanaka0523@gmail.com>
2023-05-03 11:03:07 +0000

    :recycle: fix: seo

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -31,10 +31,10 @@ const MySEO = () => (
     <meta property="og:description" content="" />
     <meta charSet="utf-8" />
     <title>naopedia</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+    <link rel="manifest" href="/img/site.webmanifest" />
     <meta name="msapplication-TileColor" content="#2b5797" />
     <meta name="theme-color" content="#ffffff" />
   </Head>
